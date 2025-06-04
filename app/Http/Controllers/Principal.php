59e8325a65836565cbb13b4c9865c8660c9f5132
Rafael1172 <rafaelleme050108@gmail.com>
2025-06-04 16:25:53 -0300

    aaadsadas

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -9,8 +9,4 @@ class Principal extends Controller
     function principal(){
         echo 'Página Principal';
     }
-
-    function dizerOla(){
-        echo 'Olá! Como vai?';
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,22 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-Route::get('/dizer-ola', [App\Http\Controllers\Principal::class, 'dizerOla']);
-Route::get('/conectar', [App\Http\Controllers\Usuario::class, 'conectar']);
-Route::get('/desconectar', [App\Http\Controllers\Usuario::class, 'desconectar']);
+
+Route::get('/veiculo/formulario', [App\Http\Controllers\VeiculoController::class, 'formulario'])->name('veiculo-formulario');
+Route::get('/veiculo/listar', [App\Http\Controllers\VeiculoController::class, 'listar'])->name('veiculo-listar');
+Route::post('/veiculo/store', [App\Http\Controllers\VeiculoController::class, 'store'])->name('veiculo-store');
+Route::get('veiculo/remove/{id}', [App\Http\Controllers\VeiculoController::class, 'remove'])->name('veiculo-remove');
+Route::get('veiculo/editar/{id}', [App\Http\Controllers\VeiculoController::class, 'editar'])->name('veiculo-editar');
+
+Route::get('/proprietario/formulario', [App\Http\Controllers\ProprietarioController::class, 'formulario'])->name('proprietario-formulario');
+Route::get('/proprietario/listar', [App\Http\Controllers\ProprietarioController::class, 'listar'])->name('proprietario-listar');
+Route::post('/proprietario/store', [App\Http\Controllers\ProprietarioController::class, 'store'])->name('proprietario-store');
+Route::get(' /proprietario/remove/{id}', [App\Http\Controllers\ProprietarioController::class, 'remove'])->name('proprietario-remove');
+Route::get(' /proprietario/editar/{id}', [App\Http\Controllers\ProprietarioController::class, 'editar'])->name('proprietario-editar');
+
+Route::get('/proprietario/formulario', [App\Http\Controllers\ProprietarioController::class, 'formulario'])->name('proprietario-formulario');
+Route::get('/proprietario/listar', [App\Http\Controllers\ProprietarioController::class, 'listar'])->name('proprietario-listar');
+Route::post('/proprietario/store', [App\Http\Controllers\ProprietarioController::class, 'store'])->name('proprietario-store');
+Route::get(' /proprietario/remove/{id}', [App\Http\Controllers\ProprietarioController::class, 'remove'])->name('proprietario-remove');
+Route::get(' /proprietario/editar/{id}', [App\Http\Controllers\ProprietarioController::class, 'editar'])->name('proprietario-editar');
 
