380be0160f94e72ccd7fd1311098416a96b15911
BrunoNunes100 <nunesbrunodeoliveira@gmail.com>
2025-08-06 16:27:51 -0300

    Social API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('usuario')->group(function(){
+    Route::post('registrar-se', [App\Http\Controllers\UsuarioController::class, 'registrar']);
+    Route::post('login', [App\Http\Controllers\UsuarioController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\UsuarioController::class, 'logout']);
+    Route::post('desativar-conta', [App\Http\Controllers\UsuarioController::class, 'desativarConta']);
+    Route::post('foto-upload', [App\Http\Controllers\UsuarioController::class, 'foto-upload']);
+    Route::post('editar', [App\Http\Controllers\UsuarioController::class, 'editar']);
+    Route::post('perfil', [App\Http\Controllers\UsuarioController::class, 'perfil']);
 });
