d6da05dccf6488a7227e7dcd1160fab806fd4bd6
Anthonyrr07 <anthony.rafael@etec.sp.gov.br>
2025-09-19 16:29:01 -0300

    rwfw

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('usuario')->group(function(){
+Route::middleware('auth:sanctum')->prefix('usuario')->group(function (){
     Route::post('registrar-se', [App\Http\Controllers\UsuarioController::class, 'registrar']);
-    Route::post('login', [App\Http\Controllers\UsuarioController::class, 'login']);
     Route::post('logout', [App\Http\Controllers\UsuarioController::class, 'logout']);
     Route::post('desativar-conta', [App\Http\Controllers\UsuarioController::class, 'desativarConta']);
     Route::post('foto-upload', [App\Http\Controllers\UsuarioController::class, 'foto-upload']);
     Route::post('editar', [App\Http\Controllers\UsuarioController::class, 'editar']);
     Route::post('perfil', [App\Http\Controllers\UsuarioController::class, 'perfil']);
 });
+Route::post('login', [App\Http\Controllers\UsuarioController::class, 'login']);
